7b4b70e32540226628df6026270e97c61672ba3c
mon5500000 <143809353+mon5500000@users.noreply.github.com>
2024-04-02 22:00:55 +0700

    Update jarmonhub.lua

--- jarmonhub.lua
+++ jarmonhub.lua
@@ -1,3 +1,25 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+if game.PlaceId ~= 537413528 then
+    local TeleportService = game:GetService("TeleportService")
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    
+    local Rejoin = coroutine.create(function()
+        local Success, ErrorMessage = pcall(function()
+            TeleportService:Teleport(game.PlaceId, LocalPlayer)
+        end)
+    
+        if ErrorMessage and not Success then
+            warn(ErrorMessage)
+        end
+    end)
+    
+    coroutine.resume(Rejoin)
+end
+
 if _G.AutoFarm then
     while true do -- This creates the infinite loop 
 
