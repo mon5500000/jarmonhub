797b5a35e514b61522ac2a1de785e6bd33beba3a
mon5500000 <143809353+mon5500000@users.noreply.github.com>
2024-04-21 10:01:00 +0700

    Update jarmonhub.lua

--- jarmonhub.lua
+++ jarmonhub.lua
@@ -1,9 +1,9 @@
 local placeId = game.PlaceId
 
 if placeId == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mon5500000/jarmonhub/main/Build-A-Boat.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/jarmonhubobf/main/Build-A-Boat.lua"))()
 elseif placeId == 3956818381 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mon5500000/jarmonhub/main/Ninja.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/jarmonhubobf/main/Ninja.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mon5500000/jarmonhub/main/jarmon.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/jarmonhubobf/main/jarmon.lua"))()
 end
