78cc26480d61e7ddeef31e6593dce1cf198f9683
mon5500000 <143809353+mon5500000@users.noreply.github.com>
2024-04-26 11:19:31 +0700

    Update jarmonhub.lua

--- jarmonhub.lua
+++ jarmonhub.lua
@@ -1,11 +1,2 @@
 setclipboard("https://discord.gg/qpA4rkrr9A")
-game.Players.LocalPlayer:Kick("กรุณาเอาสคริปตัวใหม่ด้วยที่ https://discord.gg/qpA4rkrr9A")
-local placeId = game.PlaceId
-
-if placeId == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/jarmonhubobf/main/Build-A-Boat.lua"))()
-elseif placeId == 3956818381 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/jarmonhubobf/main/Ninja.lua"))()
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/jarmonhubobf/main/jarmon.lua"))()
-end
+game.Players.LocalPlayer:Kick("กรุณาเอาสคริปตัวใหม่ด้วยที่ \nhttps://discord.gg/qpA4rkrr9A\n(เราคัดลอกสคริปท์ให้คุณแล้ว)")
